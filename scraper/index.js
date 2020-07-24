dee206bfb431c3bc871774459ddf7a879fbc9476
markus-maelzer <markus.maelzer@gmail.com>
2020-07-24 23:25:56 +0200

    finish barebones scraping

--- scraper/index.js
+++ scraper/index.js
@@ -14,7 +14,7 @@ const scrape = async () => {
 };
 
 const scrapeGeneralData = async (url) => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto(url);
 
@@ -25,11 +25,42 @@ const scrapeGeneralData = async (url) => {
 };
 
 const scrapeChapters = async (firstChapter) => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto(firstChapter);
-
   await handleCookiePopup(page);
+
+  let count = 0;
+  let nextChap = firstChapter;
+
+  try {
+    while (count < 30 && nextChap) {
+      await page.goto(nextChap);
+      const chapter = await getChapter(page);
+      console.log(chapter.title);
+      nextChap = chapter.nextChap;
+      count++;
+      await page.waitFor(5000);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const getChapter = async (page) => {
+  const result = page.evaluate(() => {
+    const title = document.querySelector('.titles h2').textContent.trim();
+    const nextChap = document.querySelector('.nextchap');
+    const content = document
+      .querySelector('.chapter-content')
+      .innerHTML.split(`<br>`);
+    return {
+      title,
+      content,
+      nextChap: nextChap.classList.contains('isDisabled') ? false : nextChap.href,
+    };
+  });
+  return result;
 };
 
 const getGeneralData = async (page) => {
