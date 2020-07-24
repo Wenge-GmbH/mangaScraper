8448007e6e5ee4ba545a42f458d1c86f46b302a3
markus-maelzer <markus.maelzer@gmail.com>
2020-07-24 23:50:12 +0200

    change code to work with babel

--- scraper/index.js
+++ scraper/index.js
@@ -14,13 +14,14 @@ const scrape = async () => {
 };
 
 const scrapeGeneralData = async (url) => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(url);
 
   await handleCookiePopup(page);
   const generalData = await getGeneralData(page);
-  browser.close();
+  console.log('hi');
+  await browser.close();
   return generalData;
 };
 
@@ -64,43 +65,43 @@ const getChapter = async (page) => {
 };
 
 const getGeneralData = async (page) => {
-  const result = await page.evaluate(() => {
-    const title = document.querySelector('h1.novel-title').textContent.trim();
-    const firstChapter = document.querySelector('a#readchapterbtn').href;
-    const author = document
-      .querySelector('.novel-info .property-item')
-      .textContent.trim();
-
-    const hope = [...document.querySelectorAll('.novel-info span')].filter((a) =>
-      a.textContent.includes('Status : ')
-    );
-    const status =
-      hope.length > 0 && hope[0].parentElement.lastElementChild.textContent;
-
-    const coverLink = document.querySelector('.cover img').src;
-    const summary = document.querySelector('.summary .content p').textContent;
-
-    const categories = [
-      ...document.querySelectorAll('.categories .content li a'),
-    ].map((el) => el.textContent.trim());
-
-    const tags = [...document.querySelectorAll('.tags .content li a')].map((el) =>
-      el.textContent.trim()
-    );
-
-    return {
-      title,
-      firstChapter,
-      author,
-      status,
-      coverLink,
-      summary,
-      categories,
-      tags,
-    };
-  });
-  console.log(result);
-  return result;
+  try {
+    const result = await page.evaluate(() => {
+      const title = document.querySelector('h1.novel-title').textContent.trim();
+      const firstChapter = document.querySelector('a#readchapterbtn').href;
+      const author = document
+        .querySelector('.novel-info .property-item')
+        .textContent.trim();
+      const hope = Array.from(
+        document.querySelectorAll('.novel-info span')
+      ).filter((a) => a.textContent.includes('Status : '));
+      const status =
+        hope.length > 0 && hope[0].parentElement.lastElementChild.textContent;
+      const coverLink = document.querySelector('.cover img').src;
+      const summary = document.querySelector('.summary .content p').textContent;
+      const categories = Array.from(
+        document.querySelectorAll('.categories .content li a')
+      ).map((el) => el.textContent.trim());
+      const tags = Array.from(
+        document.querySelectorAll('.tags .content li a')
+      ).map((el) => el.textContent.trim());
+      return {
+        title,
+        firstChapter,
+        author,
+        status,
+        coverLink,
+        summary,
+        categories,
+        tags,
+      };
+    });
+    console.log(result);
+    return result;
+  } catch (e) {
+    console.log('catch');
+    console.log(e);
+  }
 };
 
 const handleCookiePopup = async (page) => {
