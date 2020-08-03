2bec3232d416e9073685d3acf5f5b22d8ed69c98
markus-maelzer <markus.maelzer@gmail.com>
2020-08-03 23:29:02 +0200

    add user model

---
+++ src/models/User.js
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+// const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+});
+
+// On Save Hook, encrypt password
+// "pre" -> Before saving a model, run this function
+// userSchema.pre('save', function (next) {
+//   // get access to the user model
+//   const user = this;
+//   // generate a salt
+//   bcrypt.genSalt(10, function (err, salt) {
+//     if(err) {
+//       return next(err);
+//     }
+
+//     // has (encrypt) the password using salt
+//     bcrypt.hash(user.password, salt, function (err, hash) {
+//       if(err) {return next(err);}
+
+//       // overwrite plain text password with encrypted password
+//       user.password = hash;
+//       next();
+//     })
+//   })
+// })
+
+// userSchema.methods.comparePassword = function (candidatePassword, callback) {
+//   bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+//     if(err) { return callback(err); }
+
+//     callback(null, isMatch);
+//   })
+// }
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
