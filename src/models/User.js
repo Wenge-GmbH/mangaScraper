37963f0d21048663ed066cb3c25e0742b81acee5
Markus Maelzer <markus.maelzer@gmail.com>
2020-08-04 10:45:58 +0200

    add bcrypt and pw methods to model

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-// const bcrypt = require('bcryptjs');
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -27,34 +27,36 @@ const userSchema = new mongoose.Schema({
 
 // On Save Hook, encrypt password
 // "pre" -> Before saving a model, run this function
-// userSchema.pre('save', function (next) {
-//   // get access to the user model
-//   const user = this;
-//   // generate a salt
-//   bcrypt.genSalt(10, function (err, salt) {
-//     if(err) {
-//       return next(err);
-//     }
+userSchema.pre('save', function (next) {
+  // get access to the user model
+  const user = this;
+  // generate a salt
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) {
+      return next(err);
+    }
 
-//     // has (encrypt) the password using salt
-//     bcrypt.hash(user.password, salt, function (err, hash) {
-//       if(err) {return next(err);}
+    // has (encrypt) the password using salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) {
+        return next(err);
+      }
 
-//       // overwrite plain text password with encrypted password
-//       user.password = hash;
-//       next();
-//     })
-//   })
-// })
+      // overwrite plain text password with encrypted password
+      user.password = hash;
+      next();
+    });
+  });
+});
 
-// userSchema.methods.comparePassword = function (candidatePassword, callback) {
-//   bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
-//     if(err) { return callback(err); }
+userSchema.methods.comparePassword = function (candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) {
+      return callback(err);
+    }
 
-//     callback(null, isMatch);
-//   })
-// }
+    callback(null, isMatch);
+  });
+};
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+export default User;
